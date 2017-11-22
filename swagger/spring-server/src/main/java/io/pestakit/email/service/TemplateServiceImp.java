0d6892dab105830ec3c909832b2b1c6f33c4e2e2
CoolPolishGuy <wojciech.myszkorowski@heig-vd.ch>
2017-11-22 16:21:56 +0100

    update de la signature des fonctions

--- swagger/spring-server/src/main/java/io/pestakit/email/service/TemplateServiceImp.java
+++ swagger/spring-server/src/main/java/io/pestakit/email/service/TemplateServiceImp.java
@@ -1,13 +1,22 @@
 package io.pestakit.email.service;
 
 import io.pestakit.email.api.model.Email;
+import org.springframework.mail.SimpleMailMessage;
 
 import java.util.Map;
 
 public class TemplateServiceImp implements TemplateService {
     @Override
-    public Email createTemplate(Email email, String url, Map<String,String> ableau) {
-        Email email1 = new Email();
+    public SimpleMailMessage createTemplate(Email emailreciev) {
+        SimpleMailMessage email = new SimpleMailMessage();
+
+        email.setFrom(emailreciev.getSender());
+        email.setTo(emailreciev.getRecipients().toArray(new String[emailreciev.getRecipients().size()]));
+        email.setCc(emailreciev.getCarbonCopy().toArray(new String[emailreciev.getCarbonCopy().size()]));
+        email.setBcc(emailreciev.getBlindCarbonCopy().toArray(new String[emailreciev.getBlindCarbonCopy().size()]));
+        email.setSubject(email.getSubject());
+
+
 
         return email;
 
