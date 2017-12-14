e2edc80d10ab17fa278880ab006dafc2fa101d5f
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2017-12-14 01:33:04 +0100

    Test TemplatesSteps worked some times, need more time

--- swagger/email-specs/src/test/java/io/pestakit/email/api/spec/steps/TemplatesSteps.java
+++ swagger/email-specs/src/test/java/io/pestakit/email/api/spec/steps/TemplatesSteps.java
@@ -7,14 +7,28 @@ import cucumber.api.java.en.When;
 import io.pestakit.email.ApiException;
 import io.pestakit.email.ApiResponse;
 import io.pestakit.email.api.DefaultApi;
+import io.pestakit.email.api.dto.Tag;
 import io.pestakit.email.api.dto.Template;
 import io.pestakit.email.api.spec.helpers.Environment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class TemplatesSteps {
 
+    private final List<Long> tagsID = new ArrayList<>();
+
     private Template template;
+    private Long id;
+    private String name;
+    private List<String> tags;
+    private String body;
+
+    private int numberOfTemplates;
 
     private Environment environment;
     private DefaultApi api;
@@ -29,25 +43,67 @@ public class TemplatesSteps {
         this.api = environment.getApi();
     }
 
-    @Given("^There is a Emails server$")
-    public void there_is_a_Emails_server() throws Throwable {
+    @Given("^There is a template api$")
+    public void thereIsATemplateApi() throws Throwable {
         assertNotNull(api);
     }
 
-    @And("^There is an empty database$")
-    public void there_is_an_empty_database() throws Throwable {
-        assertNull(lastApiResponse.getData());
+    @And("^I get the number of templates$")
+    public void iGetTheNumberOfTemplates() throws Throwable {
+        numberOfTemplates = api.getTemplates().size();
     }
 
-    @Given("^I have a template payload$")
-    public void i_have_a_template_payload() throws Throwable {
+    @Given("^I have a template object$")
+    public void iHaveATemplateObject() throws Throwable {
         template = new Template();
-        template.setBody("Bonjour, @Title @FirstName @LastName, comment allez vous ?");
-        template.setName("TemplateBonjour");
+        assertNotNull(template);
+    }
+
+    @And("^I set a name$")
+    public void iSetAName() throws Throwable {
+        name = "Caco-Calo";
+        template.setName(name);
+        assertEquals(name, template.getName());
+    }
+
+    @And("^I have created (\\d+) tags$")
+    public void iHaveCreatedTags(int arg0) throws Throwable {
+        int oldNumberOfTags = api.getTags().size();
+
+        Tag tag = new Tag();
+        tag.setName("Food");
+        api.createTag(tag);
+        tagsID.add(oldNumberOfTags + 1L);
+
+        tag.setName("Drink");
+        api.createTag(tag);
+        tagsID.add(oldNumberOfTags + 2L);
+
+        tag.setName("Advertising");
+        api.createTag(tag);
+        tagsID.add(oldNumberOfTags + 3L);
+
+        assertEquals(arg0, api.getTags().size() - oldNumberOfTags);
+    }
+
+    @And("^I set tags$")
+    public void iSetTags() throws Throwable {
+        tags = Arrays.asList(api.getTag(tagsID.get(0)).getUrl(), api.getTag(tagsID.get(1)).getUrl());
+        template.setTags(tags);
+        assertEquals(tags, template.getTags());
+    }
+
+    @And("^I set a body$")
+    public void iSetABody() throws Throwable {
+        body = "<span th:text=\\\" 'Hello ' + ${title}  + ',\n" +
+                "My name is ' + ${firstName} + '  ' + ${lastName} + '.\n'" +
+                "\\\"></span>";
+        template.setBody(body);
+        assertEquals(body, template.getBody());
     }
 
     @When("^I POST it to the /templates endpoint$")
-    public void i_POST_it_to_the_templates_endpoint() throws Throwable {
+    public void iPOSTItToTheTemplatesEndpoint() throws Throwable {
         try {
             lastApiResponse = api.createTemplateWithHttpInfo(template);
             lastApiCallThrewException = false;
@@ -62,12 +118,17 @@ public class TemplatesSteps {
     }
 
     @Then("^I receive a (\\d+) status code$")
-    public void i_receive_a_status_code(int arg0) throws Throwable {
+    public void iReceiveAStatusCode(int arg0) throws Throwable {
         assertEquals(arg0, lastStatusCode);
     }
 
-    @When("^I GET /templates$")
-    public void i_GET_templates() throws Throwable {
+    @And("^The number of templates was increment of (\\d+)$")
+    public void theNumberOfTemplatesWasIncrementOf(int arg0) throws Throwable {
+        assertEquals(arg0, api.getTemplates().size() - numberOfTemplates);
+    }
+
+    @When("^I GET all templates to the /templates endpoint$")
+    public void iGETAllTemplatesToTheTemplatesEndpoint() throws Throwable {
         try {
             lastApiResponse = api.getTemplatesWithHttpInfo();
             lastApiCallThrewException = false;
@@ -82,7 +143,127 @@ public class TemplatesSteps {
     }
 
     @And("^Response body should contain all templates$")
-    public void response_body_should_contain_all_templates() throws Throwable {
+    public void responseBodyShouldContainAllTemplates() throws Throwable {
         assertNotNull(lastApiResponse.getData());
+    }
+
+    @And("^The number of templates as same before$")
+    public void theNumberOfTemplatesAsSameBefore() throws Throwable {
+        assertEquals(api.getTemplates().size(), numberOfTemplates);
+    }
+
+    @Given("^I get ID of last template$")
+    public void iGetIDOfLastTemplate() throws Throwable {
+        id = (long) api.getTemplates().size();
+    }
+
+    @When("^I GET a template with ID to the /template/id endpoint$")
+    public void iGETATemplateWithIDToTheTemplateIdEndpoint() throws Throwable {
+        try {
+            lastApiResponse = api.getTemplateWithHttpInfo(id);
+            lastApiCallThrewException = false;
+            lastApiException = null;
+            lastStatusCode = lastApiResponse.getStatusCode();
+        } catch (ApiException e) {
+            lastApiCallThrewException = true;
+            lastApiResponse = null;
+            lastApiException = e;
+            lastStatusCode = lastApiException.getCode();
+        }
+    }
+
+    @And("^Response body should contain a template$")
+    public void responseBodyShouldContainATemplate() throws Throwable {
+        template = (Template) lastApiResponse.getData();
+        assertNotNull(template);
+    }
+
+    @And("^ID is same as before$")
+    public void idIsSameAsBefore() throws Throwable {
+        assertEquals((long) id, numberOfTemplates + 1);
+    }
+
+    @And("^name is same as before$")
+    public void nameIsSameAsBefore() throws Throwable {
+        assertEquals(name, template.getName());
+    }
+
+    @And("^tags are same as before$")
+    public void tagsAreSameAsBefore() throws Throwable {
+        assertEquals(tags, template.getTags());
+    }
+
+    @And("^body is same as before$")
+    public void bodyIsSameAsBefore() throws Throwable {
+        assertEquals(body, template.getBody());
+    }
+
+    @And("^parameters aren't null$")
+    public void parametersArenTNull() throws Throwable {
+        assertNotNull(template.getParameters());
+    }
+
+    @And("^parameters aren't empty$")
+    public void parametersArenTEmpty() throws Throwable {
+        assertFalse(template.getParameters().isEmpty());
+    }
+
+    @And("^I change the name$")
+    public void iChangeTheName() throws Throwable {
+        name = "Rud-Bell";
+        template.setName(name);
+        assertEquals(name, template.getName());
+    }
+
+    @And("^I change tags$")
+    public void iChangeTags() throws Throwable {
+        tags = Collections.singletonList(api.getTag(tagsID.get(2)).getUrl());
+        template.setTags(tags);
+        assertEquals(tags, template.getTags());
+    }
+
+    @And("^I change the body$")
+    public void iChangeTheBody() throws Throwable {
+        body = "<span th:text=\\\" 'Hello ' + ${title}  + ',\n" +
+                "My name is ' + ${firstName} + '  ' + ${lastName} + ', how are you today ?\n" +
+                "It's a beautiful day !'" +
+                "\\\"></span>";
+        template.setBody(body);
+        assertEquals(body, template.getBody());
+    }
+
+    @When("^I PUT a template with ID to the /templates/id endpoint$")
+    public void iPUTATemplateWithIDToTheTemplatesIdEndpoint() throws Throwable {
+        try {
+            lastApiResponse = api.updateTemplateWithHttpInfo(String.valueOf(id), template);
+            lastApiCallThrewException = false;
+            lastApiException = null;
+            lastStatusCode = lastApiResponse.getStatusCode();
+        } catch (ApiException e) {
+            lastApiCallThrewException = true;
+            lastApiResponse = null;
+            lastApiException = e;
+            lastStatusCode = lastApiException.getCode();
+        }
+    }
+
+    @When("^I DELETE a template with ID to the /templates/id endpoint$")
+    public void iDELETEATemplateWithIDToTheTemplatesIdEndpoint() throws Throwable {
+        try {
+            lastApiResponse = api.deleteTemplateWithHttpInfo(id);
+            lastApiCallThrewException = false;
+            lastApiException = null;
+            lastStatusCode = lastApiResponse.getStatusCode();
+        } catch (ApiException e) {
+            lastApiCallThrewException = true;
+            lastApiResponse = null;
+            lastApiException = e;
+            lastStatusCode = lastApiException.getCode();
+        }
+    }
+
+    @And("^The number of templates was decremented of (\\d+)$")
+    public void theNumberOfTemplatesWasDecrementedOf(int arg0) throws Throwable {
+        assertEquals(arg0, numberOfTemplates - api.getTemplates().size());
     }
 }
--- swagger/email-specs/src/test/resources/scenarios/template-api-controller.feature
+++ swagger/email-specs/src/test/resources/scenarios/template-api-controller.feature
@@ -1,29 +1,60 @@
 Feature: Spring api controller template
 
   Background:
-    Given There is a Emails server
+    Given There is a template api
+    And I get the number of templates
 
   Scenario: Create a template
-    Given I have a template payload
+    Given I have a template object
+    And I set a name
+    And I have created 3 tags
+    And I set tags
+    And I set a body
     When I POST it to the /templates endpoint
     Then I receive a 201 status code
-
-  Scenario: Create a template
-    Given I have a template payload
-    When I POST it to the /templates endpoint
-    Then I receive a 201 status code
+    And The number of templates was increment of 1
 
   Scenario: Get all templates
-    When I GET /templates
+    When I GET all templates to the /templates endpoint
     Then I receive a 200 status code
     And Response body should contain all templates
+    And The number of templates as same before
 
-#  Scenario: Get a template
-#    When I GET /template
-#    Then I receive a 200 status code
-#    And Response body should contain a template
+  Scenario: Get a template
+    Given I have a template object
+    And I set a name
+    And I have created 3 tags
+    And I set tags
+    And I set a body
+    When I POST it to the /templates endpoint
+    Then I receive a 201 status code
+    And The number of templates was increment of 1
+    And I get ID of last template
+    When I GET a template with ID to the /template/id endpoint
+    Then I receive a 200 status code
+    And Response body should contain a template
+    And ID is same as before
+    And name is same as before
+    And tags are same as before
+    And body is same as before
+    And parameters aren't null
+    And parameters aren't empty
 
-  #Scenario: Delete a template
-  #  When I DELETE a template to the /templates/$.id endpoint
-  #  Then I receive a 200 status code
-  #  And  The template doesn't exist anymore
+  Scenario: Update a template
+    Given I get ID of last template
+    And I GET a template with ID to the /template/id endpoint
+    And Response body should contain a template
+    And I change the name
+    And I have created 3 tags
+    And I change tags
+    And I change the body
+    When I PUT a template with ID to the /templates/id endpoint
+    Then I receive a 200 status code
+
+  Scenario: Delete a template
+    Given I get ID of last template
+    When I DELETE a template with ID to the /templates/id endpoint
+    Then I receive a 200 status code
+    And The number of templates was decremented of 1
+    When I GET a template with ID to the /template/id endpoint
+    Then I receive a 500 status code
